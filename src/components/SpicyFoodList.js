fe6bd6c96a55d7619436ba698e9999d54ed0853e
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-11-11 04:37:47 +0300

    Done Coding ALong

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,25 +1,90 @@
 import React, { useState } from "react";
-import { spicyFoods, getNewRandomSpicyFood } from "../data";
+import { spicyFoods, getNewRandomSpicyFood } from "../data/Data";
 
+//Adding Elements To Arrays In State
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All")
+  const [foods, setFoods] = useState(spicyFoods);  
 
-  function handleAddFood() {
+ 
+
+//console.log("Foods:", foods) => foods = spicyFoods
+  const handleAddFood = () => {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray);
   }
 
-  const foodList = foods.map((food) => (
+  //Removing elements fro array in State
+  const handleLiClick = (id) => {//Function that takes an id as an argument
+    //[1, 2, 3].filter((number) => number !== 3);  => [1,2] All numbers except 3.
+    //const removedFood = foods.filter((food) => food.id !== id) //All foods except the one we are removing    
+    //setFoods(removedFood) //Update the state of the food
+    const heatArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(heatArray);
+  }
+
+  const handleDelete = (id) => {
+    //[1, 2, 3].filter((number) => number !== 3);  => [1,2] All numbers except 3.
+    const removedFood = foods.filter((food) => food.id !== id) //All foods except the one we are removing    
+    setFoods(removedFood) //Update the state of the food
+  } 
+
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
+
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+
+
+
+  const foodList = foodsToDisplay.map((food) => (
     <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      {food.name} | Heat: <button onClick={() => handleLiClick(food.id)}>{food.heatLevel} </button>| Cuisine: {food.cuisine}
+      <button onClick={() => handleDelete(food.id)}> Delete</button>
     </li>
   ));
 
+
+
+  
+  
+
+  
   return (
     <div>
+       <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
+      
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <ul></ul>
+
+     
     </div>
+
+    
   );
 }
 
